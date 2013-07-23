ac7abd33b8e214e80b6b2c9a6f54c46b3df9fe99
Martin Fournier <martinfournier@scolab.com>
2013-07-23 17:12:10 -0400

    Initial commit (imported from bitbucket)

---
+++ static/js/centurion.index.js
@@ -0,0 +1,74 @@
+﻿/// <reference path="../Libs/jquery-1.5.1-vsdoc.js" />
+
+var secondsPerTick = 60;
+var intervalShots = secondsPerTick * 1000;
+
+var timeRem = secondsPerTick;
+var nbShotsTaken = 0;
+var nbShotsToTake = 100;
+var paused = false;
+
+function initialize() {
+    $(".game").hide();
+    $("#aStart").click(function () { return startGame(); });
+    $("#aPause").click(function () { return togglePauseGame(); });
+    $("#aUnpause").click(function () { return togglePauseGame(); });  
+}
+
+function startGame() {
+    nbShotsTaken = $("#txtNbShotsStart").val();
+    setInterval(shotTick, intervalShots);
+    setInterval(decreaseTime, 1000);
+    setShotsText();
+    setTimeText();
+    $(".start").hide();
+    $(".game").show();
+    $("#aUnpause").hide();
+    return false;
+}
+
+function togglePauseGame() {
+    paused = !paused;
+
+    if (paused) {
+        $("#aPause").hide();
+        $("#aUnpause").show();
+    } else {
+        $("#aPause").show();
+        $("#aUnpause").hide();
+    }
+    return false;
+}
+
+function decreaseTime() {
+    if (!(paused)) {
+        timeRem--;
+        setTimeText();
+    }
+}
+
+function setTimeText() {
+    var rem = timeRem;
+    if (rem < 10) { rem = '0' + rem; }
+    $("#timeRemaining").text(rem);
+}
+
+function setShotsText() {
+    $("#shotsTaken").text(nbShotsTaken + '/' + nbShotsToTake);
+}
+
+function shotTick() {
+    if (!(paused)) {
+        $("#song").get(0).play();
+        nbShotsTaken++;
+        timeRem = secondsPerTick;
+        $("#shotsTaken").text(nbShotsTaken + '/' + nbShotsToTake);
+        checkVictory();
+    }
+}
+
+function checkVictory() {
+    return false;
+}
+
+$(document).ready(function () { initialize(); });
---
+++ static/js/centurion.mobile.js
@@ -0,0 +1,82 @@
+﻿/// <reference path="../Libs/jquery-1.5.1-vsdoc.js" />
+
+var secondsPerTick = 60;
+var intervalShots = secondsPerTick * 1000;
+
+var timeRem = secondsPerTick;
+var nbShotsTaken = 0;
+var nbShotsToTake = 100;
+var paused = false;
+var initialized = false;
+
+function initialize() {
+    if (!(initialized)) {
+        $("#aStart").click(function () { return startGame(); });
+        $('#pauser').change(function (event, ui) { togglePauseGameMobile(); });
+        initialized = true;
+    }
+}
+
+function startGame() {
+    setInterval(shotTick, intervalShots);
+    setInterval(decreaseTime, 1000);
+    setShotsText();
+    setTimeText();
+    $(".start").hide();
+    $(".game").show();
+    $("#aUnpause").hide();
+    return false;
+}
+
+function togglePauseGameMobile() {
+    paused = !paused;
+    return false;
+}
+
+function decreaseTime() {
+    if (!(paused)) {
+        timeRem--;
+        setTimeText();
+    }
+}
+
+function setTimeText() {
+    var rem = timeRem;
+    if (rem < 10) { rem = '0' + rem; }
+    $("#timeRemaining").text(rem);
+}
+
+function setShotsText() {
+    $("#shotsTaken").text(nbShotsTaken);
+    $("#shotsToTake").text("/" + nbShotsToTake);
+}
+
+function shotTick() {
+    if (!(paused)) {
+        $("#song").get(0).play();
+        nbShotsTaken++;
+        timeRem = secondsPerTick;
+        setShotsText();
+        checkVictory();
+    }
+}
+
+function checkVictory() {
+    return false;
+}
+
+function setBody() {
+    $('#mainContent').height($(window).height() - $("footer").height() - $("header").height() - 18 );
+    $("div.ui-page-active").trigger('pagecreate')
+}
+
+$(window).resize(function () {
+    setBody();
+});
+
+$(document).bind('pageinit', function () {
+    initialize();
+    setBody();
+});
+
+$(document).ready(function () { setShotsText(); setBody(); });
